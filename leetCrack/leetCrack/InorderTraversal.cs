5ae8ca210ae699a175774b9795304208006b6dfe
saikat <saikat2990@gmail.com>
2023-01-15 18:58:08 +0600

    inorder

---
+++ leetCrack/leetCrack/InorderTraversal.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace leetCrack
+{
+    public class Programs
+    {
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public static void TraverseData(TreeNode node, List<int> answer)
+        {
+            if (node.left != null)
+            {
+                TraverseData(node.left, answer);
+                answer.Add(node.val);
+            }
+            else
+            {
+                answer.Add(node.val);
+            }
+
+            if (node.right != null)
+            {
+                TraverseData(node.right, answer);
+            }
+
+        }
+
+        public static IList<int> InorderTraversal(TreeNode root)
+        {
+            if (root == null)
+                return new List<int>() { };
+            var answer = new List<int>();
+            TraverseData(root, answer);
+            return answer;
+        }
+
+        public static void Main1(string[] args)
+        {
+            var root = new TreeNode('A');
+            var n1 = new TreeNode('B');
+            var n2 = new TreeNode('C');
+            var n3 = new TreeNode('D');
+            var n4 = new TreeNode('E');
+
+            // setup children
+            root.left = n1;
+            root.right = n2;
+            n1.left = n3;
+            n1.right = n4;
+
+            var data = InorderTraversal(root);
+            Console.WriteLine(data);
+        }
+    }
+}
---
+++ leetCrack/leetCrack/NumIslands.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCrack
+{
+    internal class NumIslands
+    {
+        public static void DFS(char[][] grid, int rowIndex, int colIndex, int totalRow, int totalCol, int count)
+        {
+            if (grid[rowIndex][colIndex] != '1')
+                return;
+
+            grid[rowIndex][colIndex] = 'X';
+            if (colIndex + 1 < totalCol)
+                DFS(grid, rowIndex, colIndex + 1, totalRow, totalCol, count);
+            if (rowIndex + 1 < totalRow)
+                DFS(grid, rowIndex + 1, colIndex, totalRow, totalCol, count);
+            if (colIndex > 0)
+                DFS(grid, rowIndex, colIndex - 1, totalRow, totalCol, count);
+            if (rowIndex > 0)
+                DFS(grid, rowIndex - 1, colIndex, totalRow, totalCol, count);
+
+        }
+
+        public static int NumIslandss(char[][] grid)
+        {
+            int row = grid.GetLength(0);
+            int col = grid[0].Length;
+            int count = 0;
+            for (var i = 0; i < row; i++)
+            {
+                for (var j = 0; j < col; j++)
+                {
+                    if (grid[i][j] == '1')
+                        DFS(grid, i, j, row, col, count++);
+                }
+            }
+
+
+            return count;
+        }
+    }
+}
