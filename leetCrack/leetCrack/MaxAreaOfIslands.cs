a4c5007230f7d3fbff44f5b2ac00669e0308bf6f
saikat <saikat2990@gmail.com>
2023-01-29 15:21:47 +0600

    maxAreaOfIsland

---
+++ leetCrack/leetCrack/MaxAreaOfIslands.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCrack
+{
+    public class MaxAreaOfIslands
+    {
+        public static int exploreIsland(int[][] grid, int row, int col, int tolRow, int tolCol, int found)
+        {
+            if (grid[row][col] == 0)
+            {
+                return found - 1;
+            }
+            grid[row][col] = 0;
+
+            if (row + 1 < tolRow)
+                found = exploreIsland(grid, row + 1, col, tolRow, tolCol, found + 1);
+            if (col + 1 < tolCol)
+                found = exploreIsland(grid, row, col + 1, tolRow, tolCol, found + 1);
+            if (row - 1 >= 0)
+                found = exploreIsland(grid, row - 1, col, tolRow, tolCol, found + 1);
+            if (col - 1 >= 0)
+                found = exploreIsland(grid, row, col - 1, tolRow, tolCol, found + 1);
+            return found;
+        }
+
+        public static int MaxAreaOfIsland(int[][] grid)
+        {
+            var row = grid.Length;
+            if (row == 0) return 0;
+            var col = grid[0].Length;
+            var foundList = new List<int>();
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        foundList.Add(exploreIsland(grid, i, j, row, col, 1));
+                    }
+                }
+            }
+            foundList.Sort();
+            if (foundList.Count > 0)
+                return foundList[foundList.Count - 1];
+            else return 0;
+        }
+
+    }
+}
--- leetCrack/leetCrack/Program.cs
+++ leetCrack/leetCrack/Program.cs
@@ -5,112 +5,18 @@ namespace leetCrack
 {
     public class Program
     {
-       
-
-        public static List<int> getAllValues(TreeNode node,List<int> allValues)
-        {
-            allValues.Add(node.val);
-            if (node.left != null)
-                allValues = getAllValues(node.left, allValues);
-            if (node.right != null)
-                allValues = getAllValues(node.right, allValues);
-            return allValues;
-        }
-
-        public static void isItaddableOrNot(TreeNode node,int val,bool isBigCheck, List<int> changeList)
-        {
-            List<int> allVals = new List<int>();
-            
-            
-            if (isBigCheck )
-            {
-                if (node.right != null)
-                {
-                    allVals = getAllValues(node.right, allVals);
-                    foreach (var childVal in allVals)
-                    {
-                        if (childVal <= val) changeList.Add(childVal);
-                    }
-                }
-
-               
-            }
-            else{
-                if(node.left != null)
-                {
-                    allVals = getAllValues(node.left, allVals);
-                    foreach (var childVal in allVals)
-                    {
-                        if (childVal > val) changeList.Add(childVal);
-                    }
-                }
-              
-            }
-           
-        }
-
-        public static (int,int) TraverseData(TreeNode node, List<int> changeList)
-        {
-            
-            if (node.left != null)
-            {
-                isItaddableOrNot(node, node.val, false, changeList);
-                TraverseData(node.left, changeList);
-            }
-           
-            if (node.right != null)
-            {
-                isItaddableOrNot(node, node.val, true, changeList);
-                TraverseData(node.right, changeList);
-
-            }
-            if (changeList.Count > 0)
-                return changeList.Count > 1 ? (changeList[0], changeList[1]) : (node.val, changeList[0]);
-            return (-1, -1);
-        }
-
-        private static void interChangeValue(TreeNode node,int parent,int child)
-        {
-            Console.WriteLine(parent+" "+child+" "+node?.val);
-            if (node.val == parent)
-            {
-                node.val = child;
-            }
-            else if (node.val == child)
-            {
-                node.val = parent;
-            }
-            if(node.left!=null)interChangeValue(node.left,parent,child);
-            if(node.right!=null)interChangeValue(node.right,parent,child);
-        }
-
-        public static void IsValidBstData(TreeNode root)
-        {
-
-            List<int> changeList = new List<int>();
-            var data = TraverseData(root, changeList);
-          if (data.Item1 != -1 || data.Item2 != -1)
-              interChangeValue(root, data.Item1, data.Item2);
-        }
-
+      
         public static void Main(string[] args)
         {
-            var root = new TreeNode(2);
-            var n1 = new TreeNode(3);
-            var n2 = new TreeNode(1);
-            //var n3 = new TreeNode(2);
-           // var n4 = new TreeNode(6);
+           var found= new int[][]
+           {
+               new []{ 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },new []{ 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },new []{ 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+               new []{ 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0 },new []{ 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0 },new []{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+               new []{ 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },new []{ 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 }
+           };
+          
 
-            // setup children
-            root.left = n1;
-            root.right = n2;
-            //n2.left = n3;
-            //n2.right = n4;
-
-            IsValidBstData(root);
-
-            IsValidBSTCHeck.IsValidBST2(root);
-            Console.WriteLine();
+           
         }
     }
 }
